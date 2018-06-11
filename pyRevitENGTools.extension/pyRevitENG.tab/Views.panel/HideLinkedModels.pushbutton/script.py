c865571c126316e35b4818d50ce840554786b0fe
Pablo Derendinger <pderendinger@gmail.com>
2018-06-11 09:16:07 -0300

    moved from a diferent Repo

---
+++ pyRevitENGTools.extension/pyRevitENG.tab/Views.panel/HideLinkedModels.pushbutton/script.py
@@ -0,0 +1,36 @@
+"""
+Hide Linked Models
+Version: 1.0
+Date : 06/08/18
+Autor: Pablo Derendinger 
+pyrevit: 4.5
+"""
+
+__title__ = 'Hide Linked Models'
+
+
+from pyrevit import script
+#Import pyRevit form library
+
+import clr
+clr.AddReference('RevitAPI') 
+from Autodesk.Revit.DB import * 
+from Autodesk.Revit.DB import Category
+
+uidoc = __revit__.ActiveUIDocument
+doc = uidoc.Document
+
+logger = script.get_logger()
+logger.set_quiet_mode()
+
+collector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_RvtLinks).ToElements()
+cat = None
+for i in collector:
+    cat = i.Category
+if cat is not None:
+    t = Transaction(doc,'Hide Links')
+    t.Start()
+    doc.ActiveView.HideCategoryTemporary(cat.Id)
+    t.Commit()
+
+logger.reset_level()
---
+++ pyRevitENGTools.extension/startup.py
@@ -0,0 +1,96 @@
+"""Example of IronPython script to be executed by pyRevit on extension load
+
+The script filename must end in startup.py
+
+To Test:
+- rename file to startup.py
+- reload pyRevit: pyRevit will run this script after successfully
+  created the DLL for the extension.
+
+pyRevit runs the startup script in a dedicated IronPython engine and output
+window. Thus the startup script is isolated and can not hurt the load process.
+All errors will be printed to the dedicated output window similar to the way
+errors are printed from pyRevit commands.
+"""
+
+# with open(r'C:\Temp\test.txt', 'w') as f:
+#     f.write('test')
+
+from pyrevit.framework import clr
+
+
+clr.AddReference('RevitAPI')
+clr.AddReference('RevitAPIUI')
+clr.AddReference('AdWindows')
+clr.AddReference('UIFramework')
+clr.AddReference('UIFrameworkServices')
+clr.AddReference('Newtonsoft.Json')
+
+import Autodesk.Revit.DB.Events as Event
+
+import Autodesk.Revit.DB as DB
+import Autodesk.Revit.UI as UI
+import time
+from os import path
+
+
+
+app = __revit__.Application
+
+
+
+def SaveChangeJournal(sender, event):
+    '''Save journal of elements changed during document edition'''
+    outputString= []
+    docName = ""
+    userName = ""
+    date = int(time.time())
+    doc = __revit__.ActiveUIDocument.Document
+    comment = ""
+    category = ""
+    try:
+        docName = doc.Title
+        userName = app.Username
+    
+    except:
+        pass
+    
+    AddedElementsIds  = event.GetAddedElementIds()
+    for i in AddedElementsIds:
+        element= doc.GetElement(i)
+        categoryName = element.Category.Name
+        if  categoryName is "Pipes" or categoryName is "Duct":
+            comment = e.LookupParameter('Length').AsDouble()
+        s = "%s,%s, %s, %s, %s, %s" % (date, userName, docName,"Added",str(i),categoryName ,comment)
+        outputString.append(s)
+
+    ModifiedElementsIds  = event.GetModifiedElementIds()
+    for i in ModifiedElementsIds:
+        element= doc.GetElement(i)
+        categoryName = element.Category.Name
+        if  categoryName is "Pipes" or categoryName is "Duct":
+            comment = e.LookupParameter('Length').AsDouble()
+        s = "%s,%s, %s, %s, %s, %s" % (date, userName, docName,"Added",str(i),categoryName ,comment)
+        outputString.append(s)
+
+    ##TODO: find a way to retrieve delete element information
+    # DeletedElementsIds  = event.GetDeletedElementIds()
+    # for i in DeletedElementsIds:
+    #     s = "%s,%s, %s, %s, %s" % (date, userName, docName,"Deleted",str(i))
+    #     outputString.append(s)
+    
+    filepath = path.expanduser('~/Output.txt')
+    with open(filepath, "a") as text_file:
+        for l in outputString:
+            text_file.write(l + "\n")
+
+
+
+#Execution
+app.DocumentChanged -= SaveChangeJournal
+
+
+app.DocumentChanged += SaveChangeJournal
+
+
+print('Startup script execution test.')
